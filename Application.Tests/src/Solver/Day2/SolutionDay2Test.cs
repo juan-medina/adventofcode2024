0abb2982eb736447968341c7d22fb3142d055ee6
Juan Medina <jamedina@gmail.com>
2024-12-02 09:21:44 +0000

    day 2

---
+++ Application.Tests/src/Solver/Day2/SolutionDay2Test.cs
@@ -0,0 +1,37 @@
+﻿using Application.Solver.Day2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace Application.Tests.Solver.Day2;
+
+[TestClass]
+public class SolutionDay2Test : SolutionDay2
+{
+    private const string Part1Input = """
+                                      7 6 4 2 1
+                                      1 2 7 8 9
+                                      9 7 6 2 1
+                                      1 3 2 4 5
+                                      8 6 4 4 1
+                                      1 3 6 7 9
+                                      """;
+
+    private const string Part1Expected = "2";
+    
+    [TestMethod]
+    public void ResolvePart1Test() => new SolutionDay2().Resolve(1, Part1Input).Should().Be(Part1Expected);
+
+    private const string Part2Input = """
+                                      7 6 4 2 1
+                                      1 2 7 8 9
+                                      9 7 6 2 1
+                                      1 3 2 4 5
+                                      8 6 4 4 1
+                                      1 3 6 7 9
+                                      """;
+
+    private const string Part2Expected = "4";
+
+    [TestMethod]
+    public void ResolvePart2Test() => new SolutionDay2().Resolve(2, Part2Input).Should().Be(Part2Expected);
+}
---
+++ Application/src/Solver/Day2/SolutionDay2.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.Solver.Day2;
+
+public class SolutionDay2() : DaySolver(2)
+{
+    public override string Resolve(int part, string input) => GetListFromString(input)
+        .Select(report => report.Split(" ").Select(int.Parse).ToList())
+        .Count(part == 1 ? IsLevelsValid : IsLevelValidWithDampener).ToString();
+
+    private static bool IsLevelsValid(List<int> levels) => (levels.Count < 1) ||
+                                                           levels.Zip(levels.Skip(1), (a, b) => a - b).All(diff =>
+                                                               (diff > 0) == levels[0] > levels[1] &&
+                                                               Math.Abs(diff) is (> 0 and < 4));
+
+    private static bool IsLevelValidWithDampener(List<int> levels) => IsLevelsValid(levels) ||
+                                                                      levels.Select((_, i) =>
+                                                                          levels.Where((_, index) => index != i)
+                                                                              .ToList()).Any(IsLevelsValid);
+}
