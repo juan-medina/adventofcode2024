868d7a7fad10600a97726000a544ecaf724a25a5
Juan Medina <jamedina@gmail.com>
2024-12-03 17:26:54 +0000

    3rd day solved

---
+++ Application.Tests/src/Solver/Day3/SolutionDay3Test.cs
@@ -0,0 +1,24 @@
+﻿using Application.Solver.Day3;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Application.Tests.Solver.Day3;
+
+[TestClass]
+public class SolutionDay3Test : SolutionDay3
+{
+    private const string Part1Input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+
+    private const string Part1Expected = "161";
+    
+    [TestMethod]
+    public void ResolvePart1Test() => new SolutionDay3().Resolve(1, Part1Input).Should().Be(Part1Expected);    
+    
+    private const string Part2Input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
+    private const string Part2Expected = "48";
+    
+    [TestMethod]
+    public void ResolvePart2Test() => new SolutionDay3().Resolve(2, Part2Input).Should().Be(Part2Expected);        
+    
+}
---
+++ Application/src/Solver/Day3/SolutionDay3.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Application.Solver.Day3;
+
+public partial class SolutionDay3() : DaySolver(3)
+{
+    private bool _doIt = true;
+
+    public override string Resolve(int part, string input) => OpcodesPattern().Matches(input).Select(match =>
+    {
+        switch (match.Value)
+        {
+            case "do()":
+                _doIt = true;
+                break;
+            case "don't()":
+                _doIt = false;
+                break;
+            default:
+            {
+                if (part == 1 || _doIt)
+                {
+                    return (int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
+                }
+
+                break;
+            }
+        }
+
+        return (0, 0);
+    }).Sum(value => value.Item1 * value.Item2).ToString();
+
+    [GeneratedRegex(@"mul\((\d{0,3}),(\d{0,3})\)|(do\(\))|(don\'t\(\))", RegexOptions.Multiline)]
+    private static partial Regex OpcodesPattern();
+}
