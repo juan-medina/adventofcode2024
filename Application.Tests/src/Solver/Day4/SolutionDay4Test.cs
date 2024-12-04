988d52b274b13331c751c416d6cbde7b8205f75c
Juan Medina <jamedina@gmail.com>
2024-12-04 10:08:24 +0000

    day 4 completed

---
+++ Application.Tests/src/Solver/Day4/SolutionDay4Test.cs
@@ -0,0 +1,45 @@
+﻿using Application.Solver.Day4;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Application.Tests.Solver.Day4;
+
+[TestClass]
+public class SolutionDay4Test : SolutionDay4
+{
+    private const string Part1Input = """
+                                      MMMSXXMASM
+                                      MSAMXMSMSA
+                                      AMXSXMAAMM
+                                      MSAMASMSMX
+                                      XMASAMXAMM
+                                      XXAMMXXAMA
+                                      SMSMSASXSS
+                                      SAXAMASAAA
+                                      MAMMMXMMMM
+                                      MXMXAXMASX
+                                      """;
+
+    private const string Part1Expected = "18";
+
+    [TestMethod]
+    public void ResolvePart1Test() => new SolutionDay4().Resolve(1, Part1Input).Should().Be(Part1Expected);
+    
+    private const string Part2Input = """
+                                      MMMSXXMASM
+                                      MSAMXMSMSA
+                                      AMXSXMAAMM
+                                      MSAMASMSMX
+                                      XMASAMXAMM
+                                      XXAMMXXAMA
+                                      SMSMSASXSS
+                                      SAXAMASAAA
+                                      MAMMMXMMMM
+                                      MXMXAXMASX
+                                      """;
+
+    private const string Part2Expected = "9";
+
+    [TestMethod]
+    public void ResolvePart2Test() => new SolutionDay4().Resolve(2, Part2Input).Should().Be(Part2Expected);    
+}
---
+++ Application/src/Solver/Day4/SolutionDay4.cs
@@ -0,0 +1,51 @@
+﻿namespace Application.Solver.Day4;
+
+public class SolutionDay4() : DaySolver(4)
+{
+    private const string Part1Search = "XMAS";
+    private const string Part2Search = "MAS";
+
+    public override string Resolve(int part, string input)
+    {
+        var map = GetListFromString(input);
+        return map
+            .SelectMany((row, y) => row.Select((c, x) => (x, y, c)))
+            .Where(t => part == 1 ? t.c == Part1Search[0] : t.c == Part2Search[1])
+            .Sum(t => part == 1 ? TimesAt(map, t.x, t.y, Part1Search) : CrossAt(map, t.x, t.y, Part2Search))
+            .ToString();
+    }
+
+    private static int TimesAt(List<string> map, int x, int y, string search) =>
+        Enumerable.Range(-1, 3)
+            .SelectMany(dx => Enumerable.Range(-1, 3).Select(dy => (dx, dy)))
+            .Sum(pair => CheckAt(map, x, y, pair.dx, pair.dy, search));
+
+    private static int CheckAt(List<string> map, int x, int y, int dx, int dy, string search) =>
+        search.All(character =>
+        {
+            if (x < 0 || y < 0 || y > map.Count - 1 || x > map[y].Length - 1)
+            {
+                return false;
+            }
+
+            if (map[y][x] != character)
+            {
+                return false;
+            }
+
+            y += dy;
+            x += dx;
+            return true;
+        })
+            ? 1
+            : 0;
+
+    private static int CrossAt(List<string> map, int x, int y, string search) =>
+        new[] { (-1, -1), (1, 1) }
+            .Any(pair => CheckAt(map, x + pair.Item1, y + pair.Item2, -pair.Item1, -pair.Item2, search) == 1) &&
+        new[] { (-1, 1), (1, -1) }
+            .Select(pair => (pair.Item2, pair.Item1))
+            .Any(pair => CheckAt(map, x + pair.Item1, y + pair.Item2, -pair.Item1, -pair.Item2, search) == 1)
+            ? 1
+            : 0;
+}
