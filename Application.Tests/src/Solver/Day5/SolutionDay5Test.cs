dad7b2fdbc57f21a0b270b833f6efe571e598d61
Juan Medina <jamedina@gmail.com>
2024-12-05 08:51:56 +0000

    day 5 part 1

---
+++ Application.Tests/src/Solver/Day5/SolutionDay5Test.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using Application.Solver.Day5;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Application.Tests.Solver.Day5;
+
+[TestClass]
+public class SolutionDay5Test : SolutionDay5
+{
+    private const string Part1Input = """
+                                      47|53
+                                      97|13
+                                      97|61
+                                      97|47
+                                      75|29
+                                      61|13
+                                      75|53
+                                      29|13
+                                      97|29
+                                      53|29
+                                      61|53
+                                      97|53
+                                      61|29
+                                      47|13
+                                      75|47
+                                      97|75
+                                      47|61
+                                      75|61
+                                      47|29
+                                      75|13
+                                      53|13
+
+                                      75,47,61,53,29
+                                      97,61,53,29,13
+                                      75,29,13
+                                      75,97,47,61,53
+                                      61,13,29
+                                      97,13,75,29,47
+                                      """;
+
+    private const int Part1Expected = 143;
+
+    [TestMethod]
+    public void ResolvePart1Test() => new SolutionDay5().Resolve(1, Part1Input).Should().Be(Part1Expected);
+
+    [TestMethod]
+    public void ParseInputTest()
+    {
+        const string testInput = """
+                                 1|23
+                                 10|4
+
+                                 10,20,30
+                                 60,50
+                                 """;
+
+        var expectedRules = new List<(int, int)> { (1, 23), (10, 4) };
+        var expectedUpdates = new List<List<int>> { new() { 10, 20, 30 }, new() { 60, 50 } };
+
+        var (rules, updates) = ParseInput(testInput);
+        
+        rules.Should().BeEquivalentTo(expectedRules);
+        updates.Should().BeEquivalentTo(expectedUpdates);
+    }
+}
---
+++ Application/src/Solver/Day5/SolutionDay5.cs
@@ -0,0 +1,73 @@
+﻿namespace Application.Solver.Day5;
+
+public class SolutionDay5() : DaySolver(5)
+{
+    public override int Resolve(int part, string input)
+    {
+        var (rules, updates) = ParseInput(input);
+        var toProcess = new List<List<int>>();
+
+        foreach (var update in updates)
+        {
+            var updateIsValid = true;
+            for (var i = 0; i < update.Count; i++)
+            {
+                var page = update[i];
+                // check pages left from i
+                var validFromLeft = true;
+                for (var j = i - 1; j > 0; j--)
+                {
+                    var otherPage = update[j];
+                    var valid = rules.Any(rule => rule.Item1 == page && rule.Item2 == otherPage);
+
+                    if (valid) continue;
+                    validFromLeft = false;
+                    break;
+                }
+
+                if (!validFromLeft) continue;
+                {
+                    // check pages right from i
+                    var validFromRight = true;
+                    for (var j = i + 1; j < update.Count; j++)
+                    {
+                        var otherPage = update[j];
+                        var valid = rules.Any(rule => rule.Item1 == page && rule.Item2 == otherPage);
+
+                        if (valid) continue;
+                        validFromRight = false;
+                        break;
+                    }
+
+                    if (validFromRight) continue;
+                    updateIsValid = false;
+                    break;
+                }
+            }
+            
+            if (updateIsValid)
+            {
+                toProcess.Add(update);
+            }
+        }
+
+        return toProcess.Sum(update => update[update.Count / 2]);
+    }
+
+    protected static (List<(int, int)>, List<List<int>>) ParseInput(string input)
+    {
+        var lines = GetListFromString(input);
+        var rules = lines
+            .Where(line => line.Contains('|'))
+            .Select(line => line.Split('|'))
+            .Select(tokens => (int.Parse(tokens[0]), int.Parse(tokens[1])))
+            .ToList();
+
+        var pages = lines
+            .Where(line => line.Contains(','))
+            .Select(line => line.Split(',').Select(int.Parse).ToList())
+            .ToList();
+
+        return (rules, pages);
+    }
+}
