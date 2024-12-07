c7e8e64b0e25d313476969b54402bd1af62d6c39
Juan Medina <jamedina@gmail.com>
2024-12-07 09:35:41 +0000

    day 7 part 1

---
+++ Application.Tests/src/Solver/Days/SolutionDay07Test.cs
@@ -0,0 +1,26 @@
+﻿using Application.Solver.Days;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Application.Tests.Solver.Days;
+
+[TestClass]
+public class SolutionDay07Test : SolutionDay07
+{
+    private const string Part1Input = """
+                                      190: 10 19
+                                      3267: 81 40 27
+                                      83: 17 5
+                                      156: 15 6
+                                      7290: 6 8 6 15
+                                      161011: 16 10 13
+                                      192: 17 8 14
+                                      21037: 9 7 18 13
+                                      292: 11 6 16 20
+                                      """;
+
+    private const int Part1Expected = 3749;
+
+    [TestMethod]
+    public void ResolvePart1Test() => new SolutionDay07().Resolve(1, Part1Input).Should().Be(Part1Expected);
+}
---
+++ Application/src/Solver/Days/SolutionDay07.cs
@@ -0,0 +1,36 @@
+﻿using Application.Helpers;
+
+namespace Application.Solver.Days;
+
+public class SolutionDay07() : DaySolver(7)
+{
+    public override ulong Resolve(int part, string input)
+    {
+        var total = 0ul;
+        foreach (var line in StringHelpers.GetListFromString(input))
+        {
+            var items = line.Split([' ', ':'], StringSplitOptions.RemoveEmptyEntries).Select(ulong.Parse).ToArray();
+            var target = items[0];
+            var result = Calc(items.Skip(1).ToArray(), 0, 0, target);
+            if (result == target)
+            {
+                total += result;
+            }
+        }
+        
+        return total;
+    }
+
+    private static ulong Calc(ulong[] items, ulong index, ulong current, ulong target)
+    {
+        if (current>=target)
+        {
+            return current;
+        }
+        if (index != (ulong)items.Length)
+            return Calc(items, index + 1, current + items[index], target) == target
+                ? target
+                : Calc(items, index + 1, current * items[index], target);
+        return current;
+    }
+}
