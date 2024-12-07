fa4896d39bca55f96af8add8350a32093c57f5b1
Juan Medina <jamedina@gmail.com>
2024-12-07 10:39:21 +0000

    Day 7 completed

--- Application.Tests/src/Solver/Days/SolutionDay07Test.cs
+++ Application.Tests/src/Solver/Days/SolutionDay07Test.cs
@@ -23,4 +23,21 @@ public class SolutionDay07Test : SolutionDay07
 
     [TestMethod]
     public void ResolvePart1Test() => new SolutionDay07().Resolve(1, Part1Input).Should().Be(Part1Expected);
+    
+    private const string Part2Input = """
+                                      190: 10 19
+                                      3267: 81 40 27
+                                      83: 17 5
+                                      156: 15 6
+                                      7290: 6 8 6 15
+                                      161011: 16 10 13
+                                      192: 17 8 14
+                                      21037: 9 7 18 13
+                                      292: 11 6 16 20
+                                      """;
+
+    private const int Part2Expected = 11387;
+
+    [TestMethod]
+    public void ResolvePart2Test() => new SolutionDay07().Resolve(2, Part2Input).Should().Be(Part2Expected);      
 }
--- Application/src/Solver/Days/SolutionDay07.cs
+++ Application/src/Solver/Days/SolutionDay07.cs
@@ -4,33 +4,30 @@ namespace Application.Solver.Days;
 
 public class SolutionDay07() : DaySolver(7)
 {
-    public override ulong Resolve(int part, string input)
+    public override ulong Resolve(int part, string input) => (from line in StringHelpers.GetListFromString(input)
+        select line.Split([' ', ':'], StringSplitOptions.RemoveEmptyEntries).Select(ulong.Parse).ToArray()
+        into items
+        let target = items[0]
+        let result = Calc(items.Skip(1).ToArray(), 0, 0, target, part == 1 ? Part1Operators : Part2Operators)
+        where result == target
+        select result).Aggregate(0ul, (current, result) => current + result);
+
+    private static ulong Calc(ulong[] items, ulong index, ulong current, ulong target, List<Operator> operators)
     {
-        var total = 0ul;
-        foreach (var line in StringHelpers.GetListFromString(input))
+        if (current >= target || index == (ulong)items.Length) return current;
+        foreach (var operation in operators)
         {
-            var items = line.Split([' ', ':'], StringSplitOptions.RemoveEmptyEntries).Select(ulong.Parse).ToArray();
-            var target = items[0];
-            var result = Calc(items.Skip(1).ToArray(), 0, 0, target);
-            if (result == target)
+            if (Calc(items, index + 1, operation(current, items[index]), target, operators) == target)
             {
-                total += result;
+                return target;
             }
         }
-        
-        return total;
-    }
 
-    private static ulong Calc(ulong[] items, ulong index, ulong current, ulong target)
-    {
-        if (current>=target)
-        {
-            return current;
-        }
-        if (index != (ulong)items.Length)
-            return Calc(items, index + 1, current + items[index], target) == target
-                ? target
-                : Calc(items, index + 1, current * items[index], target);
         return current;
     }
+
+    private delegate ulong Operator(ulong param1, ulong param2);
+
+    private static readonly List<Operator> Part1Operators = [(a, b) => a + b, (a, b) => a * b];
+    private static readonly List<Operator> Part2Operators = [..Part1Operators, (a, b) => ulong.Parse(a + b.ToString())];
 }
