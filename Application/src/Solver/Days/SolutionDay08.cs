15b1267fd99ba66694b212af859ddb551dcd7f34
Juan Medina <jamedina@gmail.com>
2024-12-08 11:56:27 +0000

    add some comments

--- Application/src/Solver/Days/SolutionDay08.cs
+++ Application/src/Solver/Days/SolutionDay08.cs
@@ -13,13 +13,13 @@ public class SolutionDay08() : DaySolver(8)
             .ToList().ForEach(pair =>
                 {
                     (int y, int x) d = (pair.a1.y - pair.a2.y, pair.a1.x - pair.a2.x);
-                    var i = 2 - part;
+                    var i = 2 - part; // part 2 antinodes will be where the antenna are, because is the complete line
                     while (true)
                     {
-                        (int y, int x) l = (d.y * i, d.x * i);
+                        (int y, int x) l = (d.y * i, d.x * i); // distance from the antenna [1-2] in this iteration
                         var from1 = CheckAntinode(pair.a1, (l.y, l.x));
                         var from2 = CheckAntinode(pair.a2, (-l.y, -l.x));
-                        if (part == 1 || !(from1 || from2)) break;
+                        if (part == 1 || !(from1 || from2)) break; // if we are OoB in both antinodes, or if part 1
                         i++;
                     }
                 }
@@ -33,8 +33,8 @@ public class SolutionDay08() : DaySolver(8)
     private bool CheckAntinode((int y, int x, char f) antenna, (int y, int x) diff)
     {
         (int y, int x) pos = (antenna.y + diff.y, antenna.x + diff.x);
-        if (pos.x < 0 || pos.x > _bounds.h - 1 || pos.y < 0 || pos.y > _bounds.w - 1) return false;
+        if (pos.x < 0 || pos.x > _bounds.h - 1 || pos.y < 0 || pos.y > _bounds.w - 1) return false; // stop OoB
         _antinodes.Add(pos);
-        return true;
+        return true; // regardless if the antinode was there or not we need to continue for part 2
     }
 }
