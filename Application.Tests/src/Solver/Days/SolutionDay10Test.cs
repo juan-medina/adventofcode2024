26c53501171acabd00e1695a328f100067d1d68e
Juan Medina <jamedina@gmail.com>
2024-12-10 08:34:47 +0000

    day 10

---
+++ Application.Tests/src/Solver/Days/SolutionDay10Test.cs
@@ -0,0 +1,32 @@
+﻿using Application.Solver.Days;
+using Application.Tests.Test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Application.Tests.Solver.Days;
+
+[TestClass]
+public class SolutionDay10Test : SolutionDay10
+{
+    private const string Input = """
+                                 89010123
+                                 78121874
+                                 87430965
+                                 96549874
+                                 45678903
+                                 32019012
+                                 01329801
+                                 10456732
+                                 """;
+
+    [TestMethod]
+    public void ResolvePart1Test() => TestHelpers.TestPart<SolutionDay10>(1, Input, 36);
+    
+    [TestMethod]
+    public void ResolvePart1TestWithFile() => TestHelpers.TestFullFile<SolutionDay10>(1, 811);
+    
+    [TestMethod]
+    public void ResolvePart2Test() => TestHelpers.TestPart<SolutionDay10>(2, Input, 81);
+    
+    [TestMethod]
+    public void ResolvePart2TestWithFile() => TestHelpers.TestFullFile<SolutionDay10>(2, 1794);
+}
---
+++ Application/src/Solver/Days/SolutionDay10.cs
@@ -0,0 +1,39 @@
+﻿using Application.Helpers;
+
+namespace Application.Solver.Days;
+
+public class SolutionDay10() : DaySolver(10)
+{
+    public override ulong Resolve(int part, string input)
+    {
+        var map = StringHelpers.GetListFromStringWithBounds(input);
+        _bounds = map.bounds;
+        return (ulong)map.list.SelectMany((rb, y) => rb.Select((cb, x) => (x, y, cb))).Where(t => t.cb == '0')
+            .Select(t => (t.y, t.x)).ToHashSet().Sum(head => Walk(map.list, head, part));
+    }
+
+    private int Walk(List<string> map, (int y, int x) pos, int part)
+    {
+        var queue = new Queue<((int y, int x) cpos, int searching)>([(pos, 0)]); // where we are, what we search
+        var count = 0;
+        var visits = part == 1 ? new HashSet<(int y, int x, int searching)>() : null; // dont need visit on part 2
+        while (queue.Count > 0)
+        {
+            var (cpos, searching) = queue.Dequeue();
+            if (cpos.x < 0 || cpos.x > _bounds.w - 1 || cpos.y < 0 || cpos.y > _bounds.h - 1) continue; // OoB
+            if (int.Parse(map[cpos.y][cpos.x].ToString()) != searching) continue; // not the right number
+            if (part == 1)
+                if (!visits!.Add((cpos.y, cpos.x, searching))) // already visited (only part 1)
+                    continue;
+            if (searching != 9)
+                foreach (var dir in Directions)
+                    queue.Enqueue(((cpos.y + dir.y, cpos.x + dir.x), searching + 1));
+            else count++;
+        }
+
+        return count;
+    }
+
+    private (int h, int w) _bounds;
+    private static readonly (int y, int x)[] Directions = [(0, -1), (0, 1), (-1, 0), (1, 0)];
+}
