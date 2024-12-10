47fcbc84ef6b8ebfa46b15b875279199e166dbf0
Juan Medina <jamedina@gmail.com>
2024-12-10 09:30:09 +0000

    refactor

--- Application/src/Solver/Days/SolutionDay10.cs
+++ Application/src/Solver/Days/SolutionDay10.cs
@@ -14,26 +14,23 @@ public class SolutionDay10() : DaySolver(10)
 
     private int Walk(List<string> map, (int y, int x) pos, int part)
     {
-        var queue = new Queue<((int y, int x) cpos, int searching)>([(pos, 0)]); // where we are, what we search
+        var queue = new Queue<((int y, int x) cpos, int search)>([(pos, 0)]); // where we are, what we search
         var count = 0;
-        var visits = part == 1 ? new HashSet<(int y, int x, int searching)>() : null; // dont need visit on part 2
+        var visits = part == 1 ? new HashSet<((int y, int x) pos, int search)>() : null; // dont need visit on part 2
         while (queue.Count > 0)
         {
-            var (cpos, searching) = queue.Dequeue();
+            var (cpos, search) = queue.Dequeue();
             if (cpos.x < 0 || cpos.x > _bounds.w - 1 || cpos.y < 0 || cpos.y > _bounds.h - 1) continue; // OoB
-            if (int.Parse(map[cpos.y][cpos.x].ToString()) != searching) continue; // not the right number
+            if (int.Parse(map[cpos.y][cpos.x].ToString()) != search) continue; // not the right number
             if (part == 1)
-                if (!visits!.Add((cpos.y, cpos.x, searching))) // already visited (only part 1)
+                if (!visits!.Add((cpos, search))) // already visited (only part 1)
                     continue;
-            if (searching != 9)
-                foreach (var dir in Directions)
-                    queue.Enqueue(((cpos.y + dir.y, cpos.x + dir.x), searching + 1));
+            if (search != 9) foreach (var dir in Dirs) queue.Enqueue(((cpos.y + dir.y, cpos.x + dir.x), search + 1));
             else count++;
         }
-
         return count;
     }
 
     private (int h, int w) _bounds;
-    private static readonly (int y, int x)[] Directions = [(0, -1), (0, 1), (-1, 0), (1, 0)];
+    private static readonly (int y, int x)[] Dirs = [(0, -1), (0, 1), (-1, 0), (1, 0)];
 }
