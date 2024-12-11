6a205a5d7b3dab439b1042a5d3c59aa8ecdb3e6f
Juan Medina <jamedina@gmail.com>
2024-12-11 05:50:56 +0000

    day 11

---
+++ Application.Tests/src/Solver/Days/SolutionDay11Test.cs
@@ -0,0 +1,23 @@
+﻿using Application.Solver.Days;
+using Application.Tests.Test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Application.Tests.Solver.Days;
+
+[TestClass]
+public class SolutionDay11Test : SolutionDay11
+{
+    private const string Input = "125 17";
+
+    [TestMethod]
+    public void ResolvePart1Test() => TestHelpers.TestPart<SolutionDay11>(1, Input, 55312);
+    
+    [TestMethod]
+    public void ResolvePart1TestWithFile() => TestHelpers.TestFullFile<SolutionDay11>(1, 213625);
+    
+    [TestMethod]
+    public void ResolvePart2Test() => TestHelpers.TestPart<SolutionDay11>(2, Input, 65601038650482UL);
+    
+    [TestMethod]
+    public void ResolvePart2TestWithFile() => TestHelpers.TestFullFile<SolutionDay11>(2, 252442982856820);
+}
---
+++ Application/src/Solver/Days/SolutionDay11.cs
@@ -0,0 +1,34 @@
+﻿using Application.Helpers;
+
+namespace Application.Solver.Days;
+
+public class SolutionDay11() : DaySolver(11)
+{
+    public override ulong Resolve(int part, string input) =>
+        StringHelpers.GetListFromString(input).SelectMany(line => line.Split(' ').Select(ulong.Parse))
+            .ToList().Aggregate(0ul, (current, stone) => current + Count(stone, part == 1 ? 25ul : 75ul)); 
+
+    private ulong Count(ulong stone, ulong iterations)
+    {
+        if (iterations == 0) return 1; // no more iterations this is the last rock
+        if (_cache.ContainsKey((stone, iterations))) return _cache[(stone, iterations)]; // already calculated
+        if (stone == 0) return Count(1, iterations - 1); // if is 0 return 1
+
+        var split = Split(stone);
+        var result = split.Item2 == ulong.MaxValue // is not even digits
+            ? Count(split.Item1 * 2024, iterations - 1)  // return number by 2024
+            : Count(split.Item1, iterations - 1) + Count(split.Item2, iterations - 1); // split number in two
+        _cache[(stone, iterations)] = result; // cache result
+        return result;
+    }
+
+    private static (ulong, ulong) Split(ulong number)
+    {
+        var length = (ulong)Math.Floor(Math.Log10(number) + 1);
+        if (length % 2 != 0) return (number, ulong.MaxValue);
+        var divisor = (ulong)Math.Pow(10, length >> 1);
+        return (number / divisor, number % divisor);
+    }
+    
+    private readonly Dictionary<(ulong stone, ulong iterations), ulong> _cache = new();
+}
