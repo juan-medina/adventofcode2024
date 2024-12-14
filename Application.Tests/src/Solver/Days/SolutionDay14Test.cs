4520abffb3c9c674611c2c56078483963ab5e66f
Juan Medina <jamedina@gmail.com>
2024-12-14 08:45:44 +0000

    day 14

---
+++ Application.Tests/src/Solver/Days/SolutionDay14Test.cs
@@ -0,0 +1,36 @@
+﻿using Application.Solver.Days;
+using Application.Tests.Test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Application.Tests.Solver.Days;
+
+[TestClass]
+public class SolutionDay14Test : SolutionDay14
+{
+    private const string Input = """
+                                 p=0,4 v=3,-3
+                                 p=6,3 v=-1,-3
+                                 p=10,3 v=-1,2
+                                 p=2,0 v=2,-1
+                                 p=0,0 v=1,3
+                                 p=3,0 v=-2,-2
+                                 p=7,6 v=-1,-3
+                                 p=3,0 v=-1,-2
+                                 p=9,3 v=2,3
+                                 p=7,3 v=-1,2
+                                 p=2,4 v=2,-3
+                                 p=9,5 v=-3,-3
+                                 """;
+
+    [TestMethod]
+    public void ResolvePart1Test() => TestHelpers.TestPart<SolutionDay14>(1, Input, 12);
+
+    [TestMethod]
+    public void ResolvePart1TestWithFile() => TestHelpers.TestFullFile<SolutionDay14>(1, 229421808);
+
+    [TestMethod]
+    public void ResolvePart2Test() => TestHelpers.TestPart<SolutionDay14>(2, Input, 1);
+
+    [TestMethod]
+    public void ResolvePart2TestWithFile() => TestHelpers.TestFullFile<SolutionDay14>(2, 6577U);
+}
---
+++ Application/src/Solver/Days/SolutionDay14.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Application.Solver.Days;
+
+public partial class SolutionDay14() : DaySolver(14)
+{
+    public override ulong Resolve(int part, string input, bool test)
+    {
+        var width = test ? 11 : 101;
+        var height = test ? 7 : 103;
+        var robots = Parse(input);
+
+        return part == 1
+            ? (ulong)Move(robots, 100, width, height)
+                .Where(r => r.x != width / 2 && r.y != height / 2) // not in the cardinals
+                .GroupBy(r => (r.x > width / 2 ? 1 : -1, r.y > height / 2 ? 1 : -1)) // group by quadrant
+                .Select(g => g.Count()) // how many per quadrant
+                .Aggregate((a, b) => a * b) // multiply
+            : (ulong)Enumerable.Range(1, 10000) // from 1 to 10000 seconds
+                .Select(i => new { Index = i, Robots = Move(robots, i, width, height) }) // move the robots
+                .First(x => x.Robots.Distinct().Count() == x.Robots.Count).Index; // they do not overlap
+    }
+
+    private static List<(int x, int y)> Move(List<((int x, int y) p, (int x, int y) v)> bots,
+        int seconds, int width, int height) => bots
+        .Select(robot => ((robot.p.x + robot.v.x * seconds) % width, (robot.p.y + robot.v.y * seconds) % height))
+        .Select(pos => (pos.Item1 < 0 ? pos.Item1 + width : pos.Item1, pos.Item2 < 0 ? pos.Item2 + height : pos.Item2))
+        .ToList();
+
+    private static List<( (int x, int y) p, (int x, int y) v)> Parse(string input)
+    {
+        var matches = ParseRegex().Matches(input);
+        return matches.Select(match => (
+            (int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value)),
+            (int.Parse(match.Groups[3].Value), int.Parse(match.Groups[4].Value))
+        )).ToList();
+    }
+
+    [GeneratedRegex(@"p=(\d+),(\d+) v=([+-]?\d+),([+-]?\d+)")]
+    private static partial Regex ParseRegex();
+}
